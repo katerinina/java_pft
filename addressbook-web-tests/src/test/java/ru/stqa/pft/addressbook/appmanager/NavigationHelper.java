afb1e0dbc4b8dbf637dd79104389dfffc03643e8
katerinina <kationok@ngs.ru>
2017-03-21 10:11:03 +0300

    сделана проверка перед каждым переходом на др.страницу, действительно ли этот переход нужно делать

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -14,13 +15,25 @@ public class NavigationHelper extends HelperBase{
     }
 
     public void gotoGroupPage() {
+        if(isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))){
+            return;
+        }
         click(By.linkText("groups"));
     }
-    public void gotoHomePage() {
+
+     public void gotoHomePage() {
+        if (isElementPresent(By.id("maintable"))){
+            return;
+        }
         click(By.linkText("home"));
     }
 
     public void returnHomePage() {
+        if (isElementPresent(By.id("maintable"))){
+            return;
+        }
         click(By.linkText("home page"));
     }
 }
