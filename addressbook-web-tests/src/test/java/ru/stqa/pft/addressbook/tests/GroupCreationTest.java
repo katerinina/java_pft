a4ec185324fb1a5fd2fe607dc30091015ecfc296
katerinina <kationok@ngs.ru>
2017-03-23 10:19:18 +0300

    сделана сортировка элементов списков перед сравнением

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -4,7 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.HashSet;
+import java.util.Comparator;
 import java.util.List;
 
 public class GroupCreationTest extends TestBase{
@@ -18,19 +18,11 @@ public class GroupCreationTest extends TestBase{
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size()+1);
 
-
-
-      //  int max = after.stream().max((o1, o2) -> Integer.compare(o1.getID())).get().getID();
-
-        int max=0;
-        for (GroupData g: after){
-            if (g.getId()>max){
-                max=g.getId();
-            }
-        }
-        group.setId(max);
         before.add(group);
-        Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
+        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before,after);
 
     }
 
