f65a0ef7488ddbf3effb89edc1913f1287a15d01
katerinina <kationok@ngs.ru>
2017-08-27 13:17:48 +0300

    произведен рефакторинг кода
    выделены вспомогательные методы

--- Rambler-tests/src/test/java/ru/rambler/tests/VerifyTests.java
+++ Rambler-tests/src/test/java/ru/rambler/tests/VerifyTests.java
@@ -1,47 +1,52 @@
 package ru.rambler.tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.*;
-import static org.openqa.selenium.OutputType.*;
 
 public class VerifyTests {
     ChromeDriver wd;
-    
+
     @BeforeMethod
     public void setUp() throws Exception {
         wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        wd.get("https://id.rambler.ru/account/registration");
     }
-    
+
     @Test
     public void testVerify() {
-        wd.get("https://id.rambler.ru/account/registration");
-        wd.findElement(By.id("login.username")).click();
-        wd.findElement(By.id("login.username")).clear();
-        //wd.findElement(By.id("login.username")).sendKeys("ekaterina_samosh");
-        wd.findElement(By.id("login.username")).sendKeys("ekaterina_samoshkina");
-        wd.findElement(By.id("password.main")).click();
-        wd.findElement(By.id("password.main")).clear();
-        wd.findElement(By.id("password.main")).sendKeys("sdfget55677749");
+        type(By.id("login.username"), "ekaterina_samoshkina");
+        type(By.id("password.main"),"6574hfjruyvbnblogu");
     }
-    
+
+
+    public void type(By locator, String text) {
+        click(locator);
+        if (text != null) {
+            String existingText = wd.findElement(locator).getAttribute("value");
+            if (!text.equals(existingText)) {
+                wd.findElement(locator).clear();
+                wd.findElement(locator).sendKeys(text);
+            }
+        }
+    }
+
+    public void click(By locator) {
+        wd.findElement(locator).click();
+    }
+
     @AfterMethod
     public void tearDown() {
         wd.quit();
     }
-    
+
     public static boolean isAlertPresent(FirefoxDriver wd) {
         try {
             wd.switchTo().alert();
